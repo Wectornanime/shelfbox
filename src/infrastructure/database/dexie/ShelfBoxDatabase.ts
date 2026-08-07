c81f80270b89d287b7960d963bafc3cca7b9fa86
Wectornanime <wectornanime@gmail.com>
2026-08-06 22:35:38 -0300

    feat(infrastructure): adds the entities of table to dexie

--- src/infrastructure/database/dexie/ShelfBoxDatabase.ts
+++ src/infrastructure/database/dexie/ShelfBoxDatabase.ts
@@ -1,8 +1,14 @@
-import Dexie from "dexie";
+import Dexie, { Table } from "dexie";
 
 import { DATABASE_NAME, DATABASE_SCHEMA, DATABASE_VERSION } from "./schema";
 
+import Collection from "@/domain/entities/Collection";
+import Miniature from "@/domain/entities/Miniature";
+
 export default class ShelfBoxDatabase extends Dexie {
+  collections!: Table<Collection, string>;
+  miniatures!: Table<Miniature, string>;
+
   constructor() {
     super(DATABASE_NAME);
 
