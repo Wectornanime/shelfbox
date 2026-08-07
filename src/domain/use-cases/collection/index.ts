e8d2683caf578c96072ae25d6c09ff85a07bdc08
Wectornanime <wectornanime@gmail.com>
2026-08-06 21:17:55 -0300

    refactor(domain): adds the index to default export of use-cases

---
+++ src/domain/use-cases/collection/index.ts
@@ -0,0 +1,5 @@
+export { default as CreateCollectionCase } from "./CreateCollection.case";
+export { default as DeleteCollectionCase } from "./DeleteCollection.case";
+export { default as GetCollectionByIdCase } from "./GetCollectionById.case";
+export { default as ListCollectionCase } from "./ListCollection.case";
+export { default as UpdateCollectionCase } from "./UpdateCollection.case";
---
+++ src/domain/use-cases/miniature/index.ts
@@ -0,0 +1,6 @@
+export { default as CreateMiniatureCase } from "./CreateMiniature.case";
+export { default as DeleteMiniatureCase } from "./DeleteMiniature.case";
+export { default as GetMiniatureByIdCase } from "./GetMiniatureById.case";
+export { default as ListMiniatureCase } from "./ListMiniature.case";
+export { default as ListMiniaturesByCollectionCase } from "./ListMiniaturesByCollection.case";
+export { default as UpdateMiniatureCase } from "./UpdateMiniature.case";
