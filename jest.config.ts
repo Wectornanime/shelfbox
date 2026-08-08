6b3035fa307781720dbe466568d1de486c031a1c
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2026-08-07 21:09:05 -0300

    chore(ci): adds the github workflow
    * chore(husky): modifies the pre-push to allow others prefix
    
    * feat: install jest
    
    * chore: configure the jest
    
    * chore: configure the lint scripts of packages.json
    
    * ci: configure the github actions workflow
    
    * style: fix no used imports
    
    * chore: adds typescript check

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+
+  testEnvironment: "node",
+
+  roots: ["<rootDir>/src"],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@components/(.*)$": "<rootDir>/src/ui/components/$1",
+    "^@pages/(.*)$": "<rootDir>/src/ui/pages/$1",
+  },
+
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+
+  clearMocks: true,
+};
+
+export default config;
