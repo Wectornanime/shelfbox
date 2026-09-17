ccdef37134462f02ed5ac6e1aa1100f7e786138e
Wectornanime <wectornanime@gmail.com>
2026-09-16 21:51:00 -0300

    feat: adds the image storage repository from dexie

--- src/infrastructure/repositories/dexie/index.ts
+++ src/infrastructure/repositories/dexie/index.ts
@@ -1,2 +1,3 @@
 export { default as DexieCollectionRepository } from "./DexieCollection.repository";
 export { default as DexieMiniatureRepository } from "./DexieMiniature.repository";
+export { default as DexieImageStorage } from "./DexieImageStorage.repository";
