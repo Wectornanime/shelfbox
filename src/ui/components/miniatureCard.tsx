ffa597d55f63f862de566f384bf72a977f9d9731
Wectornanime <wectornanime@gmail.com>
2026-09-17 21:43:37 -0300

    refactor: adds the image preview of miniatures on list

--- src/ui/components/miniatureCard.tsx
+++ src/ui/components/miniatureCard.tsx
@@ -1,20 +1,27 @@
 import { Card, CardHeader, CardTitle } from "@heroui/react";
 import { Link } from "react-router-dom";
 
+import Image from "@/domain/value-objects/Image";
+
 interface MiniatureCardProps {
   id: string;
   title: string;
+  image: Image;
 }
 
-export default function MiniatureCard({ id, title }: MiniatureCardProps) {
+export default function MiniatureCard({
+  id,
+  image,
+  title,
+}: MiniatureCardProps) {
   return (
     <Link to={`miniatures/${id}`}>
       <Card className="flex w-24 gap-2 p-0 rounded-md">
         <img
-          alt="Indie Hackers community"
+          alt={image?.alt ?? "Miniature preview"}
           className="pointer-events-none aspect-square w-24 rounded-md object-cover select-none"
           loading="lazy"
-          src="https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/docs/demo1.jpg"
+          src={image?.path ?? "/no-image-found-360x250.png"}
         />
         <CardHeader className="px-2 pb-1">
           <CardTitle>{title}</CardTitle>
